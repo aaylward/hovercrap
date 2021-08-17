243dc06f36edac43aef911cc57eaa6770b983338
Andy Aylward <aaylward@gmail.com>
2021-08-17 09:33:02 -0400

    :computer:

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function(){
   const ship = document.getElementById("ship");
-  setInterval(500, function() {
+  setInterval(function() {
     ship.classList.toggle("down-a-bit")
-  });
+  }, 500);
 })()
 
