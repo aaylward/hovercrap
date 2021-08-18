80ff57fbbf71b5f6b1572e1bd24e24e237d5cbd3
Andy Aylward <aaylward@gmail.com>
2021-08-17 21:56:34 -0400

    whooops

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 (function(){
   const frames = document.getElementsByTagName("div");
+  const length = frames.length;
   let current = 0;
   setInterval(function() {
     frames[current].classList.toggle("active")
     current++;
+    current %= length;
     frames[current].classList.toggle("active")
   }, 125);
 })()
